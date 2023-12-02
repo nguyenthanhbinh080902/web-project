eeb2dea9cf2a53b847a71aa2814ca361181329bd
nguyenthanhbinh080902 <nguyenthanhbinh080902@gmail.com>
2023-12-02 21:01:24 +0700

    working...

--- index.php
+++ index.php
@@ -4,13 +4,80 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Web phụ kiện điện thoại</title>
 </head>
 <body>
-    <div class="header">header</div>
-    <div class="menu">menu</div>
-    <div class="sidebar">sidebar</div>
-    <div class="footer">footer</div>
+    <div class="wrapper">
+        <div class="header"></div>
+        <div class="menu">
+            <ul class="list_menu">
+                <li><a href="index.php">Trang chủ</a></li>
+                <li><a href="#">Danh mục sản phẩm</a></li>
+                <li><a href="#">Giỏ hàng</a></li>
+                <li><a href="#">Tin tức</a></li>
+                <li><a href="#">Liên hệ</a></li>
+            </ul>
+        </div>
+        <div class="main">
+            <div class="sidebar">
+                <ul class="list_sidebar">
+                    <li><a href="#">Ốp lưng</a></li>
+                    <li><a href="#">Tai nghe</a></li>
+                    <li><a href="#">Cục sạc</a></li>
+                    <li><a href="#">Gậy tự sướng</a></li>
+                    <li><a href="#">Lót bàn phím</a></li>
+                </ul>
+            </div>
+            <div class="maincontent">
+                <ul class="product_list">
+                    <li>
+                        <a href="">
+                            <img src="images/tainghe1.jpg" alt="">
+                            <p class="title_product">Tên sản phẩm: Tai nghe</p>
+                            <p class="price_product">Giá : 300.000 vnd</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="images/tainghe1.jpg" alt="">
+                            <p class="title_product">Tên sản phẩm: Tai nghe</p>
+                            <p class="price_product">Giá : 300.000 vnd</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="images/tainghe1.jpg" alt="">
+                            <p class="title_product">Tên sản phẩm: Tai nghe</p>
+                            <p class="price_product">Giá : 300.000 vnd</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="images/tainghe1.jpg" alt="">
+                            <p class="title_product">Tên sản phẩm: Tai nghe</p>
+                            <p class="price_product">Giá : 300.000 vnd</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="images/tainghe1.jpg" alt="">
+                            <p class="title_product">Tên sản phẩm: Tai nghe</p>
+                            <p class="price_product">Giá : 300.000 vnd</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="images/tainghe1.jpg" alt="">
+                            <p class="title_product">Tên sản phẩm: Tai nghe</p>
+                            <p class="price_product">Giá : 300.000 vnd</p>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="clear"></div>
+        <div class="footer"></div>
+    </div>
 </body>
 </html>
